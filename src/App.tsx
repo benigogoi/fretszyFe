4b1ca7833505cb9a8905d9d15572b179fe48ab12
Beni Gogoi <beni.gogoi1@gmail.com>
2025-03-24 15:24:02 +0530

    Fixed 12th fret on mobile, add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 /// <reference types="node" />
 import { useState, useEffect } from 'react';
-import { Analytics } from "@vercel/analytics/react"
 import './App.css';
 import ResponsiveFretboard from './components/core/ResponsiveFretboard';
 import { NoteData } from './components/core/NoteMarkers';
