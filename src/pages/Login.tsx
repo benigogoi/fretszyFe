282b21177a5e1cecac25728097d7e62b9a4452cd
Beni Gogoi <beni.gogoi1@gmail.com>
2025-04-24 16:07:52 +0530

    Fixed login styling

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -186,6 +186,23 @@ const Login: React.FC = () => {
           </div>
         )}
 
+        {/* Social login options */}
+        <div className="mt-6">
+          <AuthButtons mode="vertical" className="w-full" />
+        </div>
+
+        {/* Or separator */}
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <div className="w-full border-t border-gray-300" />
+          </div>
+          <div className="relative flex justify-center text-sm">
+            <span className="px-2 bg-white text-gray-500">
+              Or continue with
+            </span>
+          </div>
+        </div>
+        
         {/* Login Form */}
         <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
           <div className="rounded-md shadow-sm -space-y-px">
@@ -303,23 +320,6 @@ const Login: React.FC = () => {
             </button>
           </div>
         </form>
-
-        {/* Or separator */}
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <div className="w-full border-t border-gray-300" />
-          </div>
-          <div className="relative flex justify-center text-sm">
-            <span className="px-2 bg-white text-gray-500">
-              Or continue with
-            </span>
-          </div>
-        </div>
-
-        {/* Social login options */}
-        <div className="mt-6">
-          <AuthButtons mode="vertical" className="w-full" />
-        </div>
 
         <div className="mt-6 text-center text-sm">
           <p className="text-gray-600">
