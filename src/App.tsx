3dfa03673f080e2b4708c64de622ed2fa18ef400
Beni Gogoi <beni.gogoi1@gmail.com>
2025-04-24 17:30:26 +0530

    Added google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 // src/App.tsx
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import AppRoutes from './Router';
 import ScrollToTop from './utils/ScrollToTop';
 import { AuthProvider } from './components/auth/AuthProvider';
 import { GoogleAuthProvider } from './components/auth/GoogleAuthProvider';
+
+declare global {
+  interface Window {
+    gtag?: (...args: any[]) => void;
+  }
+}
+
+function GATracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag('event', 'page_view', {
+        page_path: location.pathname + location.search,
+      });
+    }
+  }, [location]);
+
+  return null;
+}
 
 function App() {
   return (
@@ -11,6 +32,7 @@ function App() {
       <AuthProvider>
         <Router>
           <ScrollToTop />
+          <GATracker />
           <AppRoutes />
         </Router>
       </AuthProvider>
